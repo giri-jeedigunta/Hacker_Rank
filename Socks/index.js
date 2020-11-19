897dca9ad61229af3f6216a2861c6710defd7be3
Giri Jeedigunta <giri.jeedigunta@gmail.com>
2020-11-19 10:26:19 +0530

    Socks problem

---
+++ Socks/index.js
@@ -0,0 +1,19 @@
+const matchingSocks = (socks) => {
+    let sockStack = [];
+    let counter = 0;
+    for(var i = 0; i < socks.length; i++) {
+        if(sockStack.indexOf(socks[i]) === -1) {
+            console.log('if');
+            sockStack.push(socks[i]);
+        } else {
+            console.log('else', sockStack.indexOf(socks[i]));
+            sockStack.splice(sockStack.indexOf(socks[i]), 1);
+            console.log('else arr->', sockStack);
+            counter ++;
+        }
+    }
+    console.log('arr->', sockStack);
+    console.log('counter =>', counter);    
+}
+
+matchingSocks([10,20,20,10,10,30,50,10,20]);
